6d25d70b0aea43feb3a63b6d43a0e5391375de62
Divya R <kwrdivyaraj100@gmail.com>
2023-12-04 13:32:17 +0530

    Added flex column display style

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -19,7 +19,7 @@ function Test() {
     }
     return (
         <div>
-            <form style={{display: "flex", flexDirection: "column"}}onSubmit={handleSubmit}>
+            <form style={{display: "flex", flexDirection: "column"}} onSubmit={handleSubmit}>
                 <TextField name='name' value={inputs.name} onChange={handleChange} type={'text'} sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
                 <TextField name='email' value={inputs.email} onChange={handleChange} type={'email'} sx={{ margin: 3 }} placeholder='Email' variant='standard' />
                 <TextField name='password' value={inputs.password} onChange={handleChange} type={'password'} sx={{ margin: 3 }} placeholder='Password' variant='filled' />
