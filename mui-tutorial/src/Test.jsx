649028b9f5ed45af85db9912579f5331404badee
Divya R <kwrdivyaraj100@gmail.com>
2023-12-04 13:16:05 +0530

    Material UI Form Handling With React

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
-import { TextField, Typography } from '@mui/material';
+import { TextField, Button } from '@mui/material';
 
 function Test() {
-    const [name, setName] = useState([]);
+    const [inputs, setInputs] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+    function handleChange(e) {
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(inputs);
+    }
     return (
         <div>
-            <TextField value={name} onChange={(e) => setName(e.target.value)} type={'text'} sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
-            <TextField type={'email'} sx={{ margin: 3 }} placeholder='Email' variant='standard' />
-            <TextField type={'password'} sx={{ margin: 3 }} placeholder='Password' variant='filled' />
-            <Typography>{name}</Typography>
+            <form onSubmit={handleSubmit}>
+                <TextField name='name' value={inputs.name} onChange={handleChange} type={'text'} sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
+                <TextField name='email' value={inputs.email} onChange={handleChange} type={'email'} sx={{ margin: 3 }} placeholder='Email' variant='standard' />
+                <TextField name='password' value={inputs.password} onChange={handleChange} type={'password'} sx={{ margin: 3 }} placeholder='Password' variant='filled' />
+                <Button type='submit'>Submit</Button>
+            </form>
         </div>
     )
 }
